ca6c7e69354303a7410072ea01f781eaa86fdcda
Ola Wójcikowska <aleksandra.wojciowska@tutanota.com>
2021-03-06 12:22:17 +0100

    Clean code

--- app/src/main/java/com/wuujcik/todolist/ui/list/ListFragment.kt
+++ app/src/main/java/com/wuujcik/todolist/ui/list/ListFragment.kt
@@ -9,7 +9,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.paging.PagedList
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.wuujcik.todolist.R
 import com.wuujcik.todolist.persistence.Todo
@@ -51,8 +53,8 @@ class ListFragment : Fragment() {
         }
     }
 
-    override fun onPause() {
-        super.onPause()
+    override fun onStop() {
+        super.onStop()
         listViewModel.detachDatabaseReadListener()
     }
 
@@ -86,8 +88,7 @@ class ListFragment : Fragment() {
             adapter = todoListAdapter
             layoutManager = LinearLayoutManager(this.context)
         }
-
-        listViewModel.swipeToDelete.attachToRecyclerView(list_recycler_viewer)
+        swipeToDelete()
     }
 
     private fun showDialog(item: Todo) {
@@ -102,6 +103,28 @@ class ListFragment : Fragment() {
                 dialog.dismiss()
             }
             .show()
+    }
+
+    private fun swipeToDelete() {
+        ItemTouchHelper(object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int =
+                makeMovementFlags(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+
+            override fun onMove(
+                recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                (viewHolder as TodoListAdapter.TodoViewHolder).todo?.let {
+                    listViewModel.invalidateTodos()
+                    listViewModel.deleteTodo(it)
+                }
+            }
+        }).attachToRecyclerView(list_recycler_viewer)
     }
 
 
--- app/src/main/java/com/wuujcik/todolist/ui/list/ListViewModel.kt
+++ app/src/main/java/com/wuujcik/todolist/ui/list/ListViewModel.kt
@@ -4,8 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.paging.PagedList
 import androidx.paging.toLiveData
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
 import com.wuujcik.todolist.persistence.*
 import com.wuujcik.todolist.model.TodoProvider
 import javax.inject.Inject
@@ -32,24 +30,4 @@ class ListViewModel @Inject constructor(private val todoProvider: TodoProvider) 
     fun detachDatabaseReadListener() {
         todoProvider.detachDatabaseReadListener()
     }
-
-    val swipeToDelete = ItemTouchHelper(object : ItemTouchHelper.Callback() {
-        override fun getMovementFlags(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder
-        ): Int =
-            makeMovementFlags(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
-
-        override fun onMove(
-            recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ): Boolean = false
-
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            (viewHolder as TodoListAdapter.TodoViewHolder).todo?.let {
-                invalidateTodos()
-                deleteTodo(it)
-            }
-        }
-    })
 }
